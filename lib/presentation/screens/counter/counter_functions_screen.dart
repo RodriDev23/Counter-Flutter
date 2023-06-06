7a8ad0f85474dd7105858716959ba1a163d861bf
RodriDev23 <rodrigoprogramador23@outlook.com>
2023-06-05 22:19:31 -0300

    First commit

---
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class CounterFuncionsScreen extends StatefulWidget {
+  const CounterFuncionsScreen({super.key});
+
+  @override
+  State<CounterFuncionsScreen> createState() => _CounterFuncionsScreenState();
+}
+
+class _CounterFuncionsScreenState extends State<CounterFuncionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              Text(
+                clickCounter <= 1 ? ('Click') : ('Clicks'),
+                style: const TextStyle(fontSize: 25),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.refresh_outlined,
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            CustomButton(
+              icon: Icons.exposure_minus_1,
+              onPressed: () {
+                if (clickCounter == 0) return;
+                setState(() {
+                  clickCounter--;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                setState(() {
+                  clickCounter++;
+                });
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(icon),
+      elevation: 5,
+    );
+  }
+}
